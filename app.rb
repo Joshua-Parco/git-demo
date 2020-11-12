389903b4a04f8927316bf5f55e9081e59544ad64
James Yeates <jimibue@gmail.com>
2020-11-12 11:16:42 -0700

    menu class now takes hash and prints menu

--- app.rb
+++ app.rb
@@ -1,5 +1,38 @@
 require_relative "menu.rb"
+# menu1 = {
+#   message: "Please Select a option",
+#   options: ["add contact", "update contact", "list contact", "exit"],
+# }
 
-menu_test = Menu.new
+menu2 = {
+  message: "Welcome to the Casino select a game",
+  options: ["Crabs", "Poker", "High/Low", "exit"],
+}
 
-puts menu_test.class
+default_menu_hash = {
+  message: "Please Select a option",
+  options: ["option 1", "option 2", "exit"],
+}
+
+menu_test = Menu.new(default_menu_hash)
+
+menu_test.print
+
+casino_menu = Menu.new(menu2)
+casino_menu.print
+# Please Select a option
+#  1) option 1
+#  2) option 2
+#  3) exit
+
+# generally have message Please select an option/ please choose
+# list of options
+# user selecting on of those
+
+# i want list a menu using my class
+# Please Select a option
+#  1) option 1
+#  2) option 2
+#  3) exit
+
+# what kind of data struture does that look like message and options
--- menu.rb
+++ menu.rb
@@ -1,4 +1,15 @@
 class Menu
-  def initialize
+  def initialize(menu)
+    @menu = menu
+  end
+
+  # def print method
+  def print
+    # display menu message
+    puts @menu[:message]
+    # loop oveior menu options, need to add logic to add 1) as well
+    @menu[:options].each_with_index do |item, index|
+      puts "#{index + 1}) #{item}"
+    end
   end
 end
